921bc721e01aefde1fc4fba04a4079638e84dcdc
Shin Wang <shin_i_wang@Shin-Wangs-MacBook-Pro.local>
2013-08-29 21:29:05 -0500

    Nuevo intento para que el port sea establecido sin error en Heroku

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -10,9 +10,18 @@ object Boot extends App with SprayCanHttpServerApp {
   // create and start our service actor
   val service = system.actorOf(Props[ServiciosSoat], "serviciossoat")
 
-  val port = Option(System.getenv("PORT")).getOrElse("8080").toInt
+  val port = args.find(_.contains("spray.port")) match {
+    case Some(arg) =>
+        try {
+            Some(arg.split("=")(1).toInt)
+        } catch {
+            case e => None
+        }
+    case _ =>
+        None
+  }
 
   // create a new HttpServer using our handler tell it where to bind to
-  newHttpServer(service) ! Bind(interface = "localhost", port = port)
+  newHttpServer(service) ! Bind(interface = "localhost", port = port.getOrElse(8888))
 
 }
